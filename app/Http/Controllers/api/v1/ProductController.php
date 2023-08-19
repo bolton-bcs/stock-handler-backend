c6e0e82b60f473cd093bd38219e45c1259ebba35
danu <danujagreru@gmail.com>
2023-08-19 20:32:15 +0530

    change directory

--- app/Http/Controllers/api/v1/ProductController.php
+++ app/Http/Controllers/api/v1/ProductController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\api\v1;
 
+use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreproductRequest;
 use App\Http\Requests\UpdateproductRequest;
 use App\Models\Product;
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\SupplierController;
+use App\Http\Controllers\api\v1\ProductController;
+use App\Http\Controllers\api\v1\SupplierController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
