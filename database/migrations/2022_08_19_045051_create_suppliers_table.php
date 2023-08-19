fc8dce1b40476e8fce2695a9d90319a494b3de9f
danu <danujagreru@gmail.com>
2023-08-19 19:49:17 +0530

    migration tables completed

---
+++ database/migrations/2022_08_19_045051_create_suppliers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('suppliers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('supplier_name');
+            $table->string('supplier_address');
+            $table->string('supplier_number');
+            $table->string('supplier_email');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('suppliers');
+    }
+};
---
+++ database/migrations/2022_08_19_060618_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('supplier_Id')->unsigned()->index();;
+            $table->string('item_name');
+            $table->string('quantity');
+            $table->string('unit_price');
+            $table->string('status')->default("Active");
+            $table->timestamps();
+            $table->foreign('supplier_Id')
+                ->references('id')
+                ->on('suppliers');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
